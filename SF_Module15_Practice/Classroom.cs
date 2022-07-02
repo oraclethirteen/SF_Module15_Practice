1c381a103d870e936b9bf26e4bc97f88845216f5
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-07-02 19:54:12 +0300

    Update

---
+++ SF_Module15_Practice/Classroom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace SF_Module15_Practice
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
--- SF_Module15_Practice/Program.cs
+++ SF_Module15_Practice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
 
 namespace SF_Module15_Practice
 {
@@ -6,7 +8,21 @@ namespace SF_Module15_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(s => s.Students).ToArray();
         }
     }
 }
